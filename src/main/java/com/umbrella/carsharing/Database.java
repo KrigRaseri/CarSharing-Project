fa1ef3489b83fd686e6902fa7e5a8d555e0efd55
Krig Raseri <krigraseri@gmail.com>
2023-05-10 05:07:41 -0500

    Update Database.java
    Adds getConnection to connect to the database, and a createDBTable method for testing.

--- src/main/java/com/umbrella/carsharing/Database.java
+++ src/main/java/com/umbrella/carsharing/Database.java
@@ -1,4 +1,31 @@
 package com.umbrella.carsharing;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Database {
+
+    private Database() {}
+
+    public static Connection getConnection() throws SQLException{
+        Connection connection = null;
+        return DriverManager.getConnection("jdbc:h2:./src/carsharing/db/" + Main.db);
+    }
+
+    public static void createDBTable() {
+        try (Connection con = DriverManager.getConnection("jdbc:h2:./src/carsharing/db/" + Main.db);
+             Statement statement = con.createStatement()) {
+
+            statement.executeUpdate(" CREATE TABLE IF NOT EXISTS COMPANY " +
+                    " (ID INTEGER not NULL AUTO_INCREMENT, " +
+                    " NAME VARCHAR(255) UNIQUE NOT NULL, " +
+                    " PRIMARY KEY ( ID ))");
+            //statement.executeUpdate("DROP TABLE IF EXISTS COMPANY");
+            //System.out.println("Created table in given database...");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
