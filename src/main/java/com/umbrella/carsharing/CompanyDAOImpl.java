5054400da9daa638586f9a8e67496472cbd333b7
Krig Raseri <krigraseri@gmail.com>
2023-05-10 05:11:52 -0500

    Update all CRUD methods
    Updates all CRUD methods of CompanyDAOImpl.java to be functional.

--- src/main/java/com/umbrella/carsharing/CompanyDAOImpl.java
+++ src/main/java/com/umbrella/carsharing/CompanyDAOImpl.java
@@ -1,32 +1,94 @@
 package com.umbrella.carsharing;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CompanyDAOImpl implements CompanyDAO {
 
     @Override
     public List<Company> getAll() throws SQLException {
-        return null;
+        List<Company> companyList = new ArrayList<>();
+        Company company = null;
+
+        Connection con = Database.getConnection();
+        String sql = "SELECT * FROM company";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+            company = new Company();
+            company.setID(rs.getInt("ID"));
+            company.setName(rs.getString("name"));
+            companyList.add(company);
+        }
+
+        return companyList;
     }
 
     @Override
     public Company get(int id) throws SQLException {
-        return null;
+        Connection con = Database.getConnection();
+        Company company = null;
+
+        String sql = "SELECT id, name FROM company WHERE id = ?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+
+        if (rs.next()) {
+            int oid = rs.getInt("id");
+            String name = rs.getString("name");
+            company = new Company(oid, name);
+        }
+
+        return company;
     }
 
     @Override
     public int insert(Company company) throws SQLException {
-        return 0;
+        Connection con = Database.getConnection();
+        String sql = "INSERT INTO COMPANY (name) VALUES(?)";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, company.getName());
+        int result = ps.executeUpdate();
+
+        ps.close();
+        con.close();
+
+        return result;
     }
 
     @Override
     public int update(Company company) throws SQLException {
-        return 0;
+        Connection con = Database.getConnection();
+        String sql = "UPDATE company SET name = ? WHERE id = ?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setString(1, company.getName());
+        ps.setInt(2, company.getID());
+        int result = ps.executeUpdate();
+
+        ps.close();
+        con.close();
+
+        return result;
     }
 
     @Override
     public int delete(Company company) throws SQLException {
-        return 0;
+        Connection con = Database.getConnection();
+        String sql = "DELETE FROM company WHERE id = ?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, company.getID());
+        int result = ps.executeUpdate();
+
+        ps.close();
+        con.close();
+
+        return result;
     }
 }
+
