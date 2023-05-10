76e4eb117bc980824299d15eba8fdfc0acddf0f3
Krig Raseri <krigraseri@gmail.com>
2023-05-10 05:00:08 -0500

    Update JCommanderImpl
    Adds argument -databaseFileName control to choose database.

--- src/main/java/com/umbrella/carsharing/JCommanderImpl.java
+++ src/main/java/com/umbrella/carsharing/JCommanderImpl.java
@@ -1,4 +1,11 @@
 package com.umbrella.carsharing;
 
+import com.beust.jcommander.Parameter;
+
 public class JCommanderImpl {
+    @Parameter(
+            names = "-databaseFileName",
+            description = "Database file name"
+    )
+    String dbArgs;
 }
