900a1c6c16bea4578ad419303b36193098739376
Krig Raseri <krigraseri@gmail.com>
2023-05-10 05:05:03 -0500

    Update Main.java
    Adds JCommander usage to use the argument string db to choose database, and runs the menu.

--- src/main/java/com/umbrella/carsharing/Main.java
+++ src/main/java/com/umbrella/carsharing/Main.java
@@ -1,7 +1,23 @@
 package com.umbrella.carsharing;
 
-public class Main {
-    public static void main(String[] args) {
+import com.beust.jcommander.JCommander;
 
+import java.sql.SQLException;
+
+public class Main {
+    public static String db;
+
+    public static void main(String[] args) {
+        try {
+            JCommanderImpl jcc = new JCommanderImpl();
+            JCommander jc = JCommander.newBuilder().addObject(jcc).build();
+            jc.parse(args);
+            db = jcc.dbArgs == null ? "carsharing.mv.db" : jcc.dbArgs;
+
+            CarSharingMenu.menuInit();
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
