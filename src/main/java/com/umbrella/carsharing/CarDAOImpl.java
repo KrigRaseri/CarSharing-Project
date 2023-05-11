5adf113b39d9eb7ad6ba187b92ea3c17bd4cf203
Krig Raseri <krigraseri@gmail.com>
2023-05-11 03:53:12 -0500

    Add getAll to CarDAOImpl

--- src/main/java/com/umbrella/carsharing/CarDAOImpl.java
+++ src/main/java/com/umbrella/carsharing/CarDAOImpl.java
@@ -1,4 +1,31 @@
 package com.umbrella.carsharing;
 
-public class CarDAOImpl {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarDAOImpl implements CarDAO {
+
+    @Override
+    public List<Car> getAll() throws SQLException {
+        List<Car> carList = new ArrayList<>();
+        Car car = null;
+
+        Connection con = Database.getConnection();
+        String sql = "SELECT * FROM car";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+            car = new Car();
+            car.setID(rs.getInt("ID"));
+            car.setName(rs.getString("name"));
+            car.setCompany_ID(rs.getInt("company_ID"));
+            carList.add(car);
+        }
+        return carList;
+    }
 }
