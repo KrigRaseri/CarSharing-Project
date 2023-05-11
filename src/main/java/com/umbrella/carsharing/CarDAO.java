88fb24455a3d87ac16b12e7346b6f4a17414534b
Krig Raseri <krigraseri@gmail.com>
2023-05-11 03:37:40 -0500

    Update CarDAO.java

--- src/main/java/com/umbrella/carsharing/CarDAO.java
+++ src/main/java/com/umbrella/carsharing/CarDAO.java
@@ -1,4 +1,12 @@
 package com.umbrella.carsharing;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public interface CarDAO extends DAO {
+    List<Car> getAll() throws SQLException;
+    Car get(int id) throws SQLException;
+    int insert(Car car) throws SQLException;
+    int update(Car car) throws SQLException;
+    int delete(Car car) throws SQLException;
 }
