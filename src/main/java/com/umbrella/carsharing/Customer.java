37819c0b3833e2642727b6a27a7149f18db6c198
Krig Raseri <krigraseri@gmail.com>
2023-05-17 05:46:24 -0500

    Update customer class
    Updates customer with fields id, name, and rentedCarID.

--- src/main/java/com/umbrella/carsharing/Customer.java
+++ src/main/java/com/umbrella/carsharing/Customer.java
@@ -1,4 +1,14 @@
 package com.umbrella.carsharing;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
+    private int ID;
+    private String name;
+    private int rentedCarID;
 }
