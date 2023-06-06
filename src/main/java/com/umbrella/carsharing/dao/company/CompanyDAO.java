e7305fd5cf9ab0093047cef24a80be6812884542
Krig Raseri <krigraseri@gmail.com>
2023-06-05 20:48:17 -0500

    Create CompanyDAO.java

---
+++ src/main/java/com/umbrella/carsharing/dao/company/CompanyDAO.java
@@ -0,0 +1,18 @@
+package com.umbrella.carsharing.dao.company;
+
+import com.umbrella.carsharing.dao.DAO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CompanyDAO extends DAO<Company> {
+    List<Company> getAll() throws SQLException;
+
+    Company get(int companyID) throws SQLException;
+
+    int insert(Company company) throws SQLException;
+
+    int update(Company company) throws SQLException;
+
+    int delete(Company company) throws SQLException;
+}
