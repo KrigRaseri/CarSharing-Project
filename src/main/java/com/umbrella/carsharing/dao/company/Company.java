3a14dde1a728a4dbfdb61dde07980824cf14f123
Krig Raseri <krigraseri@gmail.com>
2023-06-05 20:48:15 -0500

    Create Company.java

---
+++ src/main/java/com/umbrella/carsharing/dao/company/Company.java
@@ -0,0 +1,15 @@
+package com.umbrella.carsharing.dao.company;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Company {
+    private int ID;
+    private String name;
+}
+
+
