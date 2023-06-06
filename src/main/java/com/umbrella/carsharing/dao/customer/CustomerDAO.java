a4a29464d2fab0e977e123eaca78f40e8f9416a5
Krig Raseri <krigraseri@gmail.com>
2023-06-05 20:48:26 -0500

    Create CustomerDAO.java

---
+++ src/main/java/com/umbrella/carsharing/dao/customer/CustomerDAO.java
@@ -0,0 +1,18 @@
+package com.umbrella.carsharing.dao.customer;
+
+import com.umbrella.carsharing.dao.DAO;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CustomerDAO extends DAO<Customer> {
+    List<Customer> getAll() throws SQLException;
+
+    Customer get(int customerID) throws SQLException;
+
+    int insert(Customer customer) throws SQLException;
+
+    int update(Customer customer) throws SQLException;
+
+    int delete(Customer customer) throws SQLException;
+}
