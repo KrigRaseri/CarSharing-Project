64cbd6a83a08bf8da8a31e5f264bbce32854cc66
Krig Raseri <krigraseri@gmail.com>
2023-06-08 21:12:02 -0500

    Update CreateTables.java
    Adds documentation comments and some better exception handling.

--- src/main/java/com/umbrella/carsharing/database/CreateTables.java
+++ src/main/java/com/umbrella/carsharing/database/CreateTables.java
@@ -1,58 +1,67 @@
 package com.umbrella.carsharing.database;
 
-import com.umbrella.carsharing.Main;
-
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public interface CreateTables {
 
-    static void createDBTable() {
+    /**
+     * Creates the COMPANY table in the database if it doesn't already exist.
+     */
+    static void createCompanyTable() {
         try (Connection con = HikariConnection.getConnection();
              Statement statement = con.createStatement()) {
 
-            statement.executeUpdate(" CREATE TABLE IF NOT EXISTS COMPANY " +
-                    " (ID INTEGER not NULL AUTO_INCREMENT, " +
-                    " NAME VARCHAR(255) UNIQUE NOT NULL, " +
-                    " PRIMARY KEY ( ID ))");
+            String createTableSQL = "CREATE TABLE IF NOT EXISTS COMPANY " +
+                    "(ID INTEGER not NULL AUTO_INCREMENT, " +
+                    "NAME VARCHAR(255) UNIQUE NOT NULL, " +
+                    "PRIMARY KEY (ID))";
+            statement.executeUpdate(createTableSQL);
             //statement.executeUpdate("DROP TABLE IF EXISTS COMPANY");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to create COMPANY table", e);
         }
     }
 
+    /**
+     * Creates the CAR table in the database if it doesn't already exist.
+     */
     static void createCarTable() {
         try (Connection con = HikariConnection.getConnection();
              Statement statement = con.createStatement()) {
 
-            statement.executeUpdate(" CREATE TABLE IF NOT EXISTS CAR " +
-                    " (ID INTEGER not NULL AUTO_INCREMENT PRIMARY KEY, " +
-                    " NAME VARCHAR(255) UNIQUE NOT NULL, " +
-                    " company_ID INTEGER NOT NULL, " +
-                    " isRented BOOLEAN DEFAULT FALSE, " +
-                    " CONSTRAINT fk_company FOREIGN KEY (company_ID) " +
-                    " REFERENCES COMPANY(ID) )");
+            String createTableSQL = "CREATE TABLE IF NOT EXISTS CAR " +
+                    "(ID INTEGER not NULL AUTO_INCREMENT PRIMARY KEY, " +
+                    "NAME VARCHAR(255) UNIQUE NOT NULL, " +
+                    "company_ID INTEGER NOT NULL, " +
+                    "isRented BOOLEAN DEFAULT FALSE, " +
+                    "CONSTRAINT fk_company FOREIGN KEY (company_ID) " +
+                    "REFERENCES COMPANY(ID))";
+            statement.executeUpdate(createTableSQL);
             //statement.executeUpdate("DROP TABLE IF EXISTS COMPANY");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to create CAR table", e);
         }
     }
 
+    /**
+     * Creates the CUSTOMER table in the database if it doesn't already exist.
+     */
     static void createCustomerTable() {
         try (Connection con = HikariConnection.getConnection();
              Statement statement = con.createStatement()) {
 
-            statement.executeUpdate(" CREATE TABLE IF NOT EXISTS CUSTOMER " +
-                    " (ID INTEGER not NULL AUTO_INCREMENT PRIMARY KEY, " +
-                    " name VARCHAR(255) UNIQUE NOT NULL, " +
-                    " rented_car_id INTEGER, " +
-                    " CONSTRAINT fk_customer FOREIGN KEY (rented_car_id) " +
-                    " REFERENCES CAR(ID) )");
+            String createTableSQL = "CREATE TABLE IF NOT EXISTS CUSTOMER " +
+                    "(ID INTEGER not NULL AUTO_INCREMENT PRIMARY KEY, " +
+                    "name VARCHAR(255) UNIQUE NOT NULL, " +
+                    "rented_car_id INTEGER, " +
+                    "CONSTRAINT fk_customer FOREIGN KEY (rented_car_id) " +
+                    "REFERENCES CAR(ID))";
+            statement.executeUpdate(createTableSQL);
             //statement.executeUpdate("DROP TABLE IF EXISTS COMPANY");
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Failed to create CUSTOMER table", e);
         }
     }
 }
