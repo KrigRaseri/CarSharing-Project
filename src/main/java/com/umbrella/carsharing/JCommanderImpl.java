e2340418964b6330633e11344b1edc53fa4861bf
Krig Raseri <krigraseri@gmail.com>
2023-06-08 21:12:12 -0500

    Update JCommanderImpl.java

--- src/main/java/com/umbrella/carsharing/JCommanderImpl.java
+++ src/main/java/com/umbrella/carsharing/JCommanderImpl.java
@@ -2,6 +2,9 @@ package com.umbrella.carsharing;
 
 import com.beust.jcommander.Parameter;
 
+/**
+ * Handles CLI inputs for the database file names.
+ * */
 public class JCommanderImpl {
     @Parameter(
             names = "-databaseFileName",
